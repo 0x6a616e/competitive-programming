0f7bb6aa7bc2c852d9441a93a262bfd4debc4c1c
Jan Reyes <janel.fantasma2002@gmail.com>
2023-03-24 21:43:34 -0600

    Add files via upload

--- OU1110.cpp
+++ OU1110.cpp
@@ -5,22 +5,30 @@
 using namespace __gnu_pbds;
 using namespace std;
 
-typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef tree<pair<long, int>, null_type, less<pair<long, int>>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
 
 int main()
 {
-        int N, score, acum = 0;
+        cin.tie(0);
+        ios::sync_with_stdio(0);
+        
+        int N, c;
+        long score;
+        double acum = 0;
         indexed_set rankings;
 
         cin >> N;
 
+        c = 0;
+
         for (int i = 0; i < N; i++) {
                 cin >> score;
-                rankings.insert(score);
-                acum += (rankings.size() - rankings.order_of_key(score));
+                rankings.insert({ score, c });
+                acum += (rankings.size() - rankings.order_of_key({ score, c }));
+                c++;
         }
 
-        cout << fixed << setprecision(2) << acum / (float) rankings.size() << '\n';
+        cout << fixed << setprecision(2) << acum / N << '\n';
         
         return 0;
 }
