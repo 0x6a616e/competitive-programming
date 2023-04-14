f3bc335dd60779be5cd1beb94cbdf6852b101903
Jan <janel.fantasma2002@gmail.com>
2023-04-13 20:25:16 -0600

    Renamed the function, going to optimize by not calculating every row, works rn

--- wip/OJ12146.cpp
+++ wip/OJ12146.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-int solve(vector<vector<int>> &memo, vector<int> &fila, int num_fila, int columna) {
+int solve_row(vector<vector<int>> &memo, vector<int> &fila, int num_fila, int columna) {
     if (memo[num_fila][columna] == -1) {
         if (fila.size() - columna == 3) {
-            memo[num_fila][columna] = max(fila[columna] + solve(memo, fila, num_fila, columna + 2), fila[columna + 1]);
+            memo[num_fila][columna] = max(fila[columna] + solve_row(memo, fila, num_fila, columna + 2), fila[columna + 1]);
         } else if (fila.size() - columna == 2) {
             memo[num_fila][columna] = max(fila[columna], fila[columna + 1]);
         } else if (fila.size() - columna == 1) {
             memo[num_fila][columna] = fila[columna];
         } else {
-            memo[num_fila][columna] = max(fila[columna] + solve(memo, fila, num_fila, columna + 2), fila[columna + 1] + solve(memo, fila, num_fila, columna + 3));
+            memo[num_fila][columna] = max(fila[columna] + solve_row(memo, fila, num_fila, columna + 2), fila[columna + 1] + solve_row(memo, fila, num_fila, columna + 3));
         }
     }
     return memo[num_fila][columna];
@@ -24,7 +24,7 @@ int main()
     int filas, columnas;
 
     while (cin >> filas >> columnas && !(filas == 0 && columnas == 0)) {
-        vector<vector<int>> memo(max(filas + 1, columnas + 1), vector<int>(max(filas, columnas), -1));
+        vector<vector<int>> memo(max(filas + 1, columnas), vector<int>(max(filas, columnas), -1));
         vector<vector<int>> matriz(filas, vector<int>(columnas, 0));
         vector<int> maximos(filas);
 
@@ -32,9 +32,9 @@ int main()
             for (int columna = 0; columna < columnas; columna++) {
                 cin >> matriz[fila][columna];
             }
-            maximos[fila] = solve(memo, matriz[fila], fila, 0);
+            maximos[fila] = solve_row(memo, matriz[fila], fila, 0);
         }
-        cout << solve(memo, maximos, filas, 0) << '\n';
+        cout << solve_row(memo, maximos, filas, 0) << '\n';
     }
     
     return 0;
