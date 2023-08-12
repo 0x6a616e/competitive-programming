9a4ae45383dffcd959a4303d9b9e8099df5db63f
Jan <janel.fantasma2002@gmail.com>
2023-08-11 18:40:09 -0600

    feat: terminado "tour de france"

--- done/OJ11242.cpp
+++ done/OJ11242.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+#include <iomanip>
+
 using namespace std;
 
 int main() {
@@ -13,23 +15,24 @@ int main() {
         for (int i = 0; i < f; ++i) cin >> front_cluster[i];
         for (int i = 0; i < r; ++i) cin >> rear_cluster[i];
 
-        vector<int> ratios;
+        vector<double> ratios;
 
         for (int i = 0; i < f; ++i) {
             for (int j = 0; j < r; ++j) {
-                ratios.push_back(rear_cluster[j] / front_cluster[i]);
+                ratios.push_back((double)rear_cluster[j] /
+                                 (double)front_cluster[i]);
             }
         }
 
         sort(ratios.begin(), ratios.end());
 
-        int mejor = 0;
+        double mejor = 0;
 
         for (int i = 1; i < (int)ratios.size(); ++i) {
             mejor = max(mejor, ratios[i] / ratios[i - 1]);
         }
 
-        cout << mejor << '\n';
+        cout << fixed << setprecision(2) << mejor << '\n';
     }
 
     return 0;
