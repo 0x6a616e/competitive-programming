ca27c7069e743a85057972c4c65daf8a3b3c2756
0x4a617a <janel.fantasma2002@gmail.com>
2023-08-17 21:28:19 -0600

    feat: made it constant

--- done/CF1860B.cpp
+++ done/CF1860B.cpp
@@ -3,22 +3,27 @@
 using namespace std;
 
 int main() {
-    int t;
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int t, m, k, a1, ak, tmp, n, ov;
     cin >> t;
 
     for (int i = 0; i < t; ++i) {
-        int m, k, a1, ak;
-
         cin >> m >> k >> a1 >> ak;
 
-        while (ak && m >= k) {
-            --ak;
-            m -= k;
-        }
-        int n = 0;
-        while (m > a1 && m >= k) {
-            ++n;
-            m -= k;
+        tmp = m / k;
+        tmp = min(tmp, ak);
+        m -= tmp * k;
+
+        n = 0;
+        if (m > a1) {
+            ov = m - a1;
+            tmp = ov / k;
+            if (ov % k) ++tmp;
+            if (tmp * k > m) --tmp;
+            n += tmp;
+            m -= tmp * k;
         }
         m -= a1;
 
