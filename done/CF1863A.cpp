6c98eb131d47676a57001c2036fa8bca04b3b5f2
0x4a617a <janel.fantasma2002@gmail.com>
2023-08-30 09:49:49 -0600

    feat: hecho un problema del concurso

---
+++ done/CF1863A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, a, q;
+        cin >> n >> a >> q;
+        int pendientes = n - a;
+        int online = 0;
+
+        char notif;
+        bool g = (n == a);
+        string notifs;
+        cin >> notifs;
+        for (int i = 0; i < q; ++i) {
+            notif = notifs[i];
+            if (notif == '-') {
+                --a;
+            } else {
+                ++online;
+                ++a;
+            }
+            if (a == n) g = true;
+        }
+
+        if (g)
+            cout << "YES\n";
+        else if (pendientes <= online)
+            cout << "MAYBE\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
