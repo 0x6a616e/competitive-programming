23d5ca94299735697e48bf81cf37e5a5b72e508c
Jan <janel.fantasma2002@gmail.com>
2023-08-31 09:53:12 -0600

    feat: concurso terminado

---
+++ done/CF1861A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int n = 100;
+    vector<bool> is_prime(n + 1, true);
+    is_prime[0] = is_prime[1] = false;
+    for (int i = 2; i * i <= n; i++)
+        if (is_prime[i])
+            for (int j = i * i; j <= n; j += i) is_prime[j] = false;
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string number;
+        cin >> number;
+        bool found = false;
+        for (int i = 0; i < (int)number.size() && !found; ++i) {
+            for (int j = i + 1; j < (int)number.size(); ++j) {
+                n = (number[i] - '0') * 10 + (number[j] - '0');
+                if (is_prime[n]) {
+                    cout << n << '\n';
+                    found = true;
+                    break;
+                }
+            }
+        }
+        if (!found) cout << -1 << '\n';
+    }
+
+    return 0;
+}
---
+++ done/CF1861B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        int s = (int)s1.size();
+
+        bool found = false;
+        for (int i = 0; i < s - 1 && !found; ++i)
+            if (s1[i] == '0' && s1[i + 1] == '1' && s2[i] == '0' &&
+                s2[i + 1] == '1')
+                found = true;
+
+        if (found)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
