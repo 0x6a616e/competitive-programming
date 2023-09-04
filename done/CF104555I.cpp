635589c9d788b478028e6234914e154358bbaef5
Jan <janel.fantasma2002@gmail.com>
2023-09-04 08:34:10 -0600

    feat: hecho el i del repechaje

---
+++ done/CF104555I.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int n, num;
+    cin >> n;
+    int pares = 0;
+    int inpares = 0;
+
+    long long sol = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> num;
+        if (num == 1) swap(pares, inpares);
+        if (num == 1) ++inpares;
+        if (num == 0) ++pares;
+        sol += inpares;
+    }
+
+    cout << sol << endl;
+
+    return 0;
+}
