14ead32bb6bc914c973f3eaeb4093ac5c114b9f3
0x4a617a <janel.fantasma2002@gmail.com>
2023-09-04 19:04:21 -0600

    feat: hecho tercero de hoy

---
+++ done/CF1360D.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+
+    int n, k, x, y;
+    for (int i = 0; i < t; ++i) {
+        cin >> n >> k;
+        int mejor = n;
+        for (int j = 1; j * j <= n; ++j) {
+            if (n % j == 0) {
+                x = j;
+                y = n / j;
+                if (y <= k) mejor = min(mejor, x);
+                if (x <= k) mejor = min(mejor, y);
+            }
+        }
+        cout << mejor << '\n';
+    }
+
+    return 0;
+}
