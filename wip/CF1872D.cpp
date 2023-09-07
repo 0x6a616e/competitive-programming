6b975d79c12fbe289d44bf8185de89b7fe10662c
Jan <janel.fantasma2002@gmail.com>
2023-09-07 11:09:31 -0600

    feat: estoy cerca

--- wip/CF1872D.cpp
+++ wip/CF1872D.cpp
@@ -21,16 +21,25 @@ int main() {
         x_multiplos -= interseccion;
         y_multiplos -= interseccion;
 
-        if (x % y == 0) x_multiplos = 0;
-        if (y % x == 0) y_multiplos = 0;
-
-        if (x_multiplos > 0) {
-            long long inicia_x = n - x_multiplos + 1;
-            sol += x_multiplos * inicia_x;
-            sol += ((n - inicia_x) * (n - inicia_x + 1)) / 2;
+        if (x % y == 0) {
+            x_multiplos = 0;
+            y_multiplos -= n / x;
+        }
+        if (y % x == 0) {
+            y_multiplos = 0;
+            x_multiplos -= n / y;
         }
 
-        long long termina_y = 1 + y_multiplos - 1;
+        cout << n << " " << x << " " << y << " " << x_multiplos << " "
+             << y_multiplos << endl;
+
+        if (x_multiplos) {
+            long long termina_x = x_multiplos - 1;
+            sol += n * x_multiplos;
+            sol -= (termina_x * (termina_x + 1)) / 2;
+        }
+
+        long long termina_y = y_multiplos;
         sol -= (termina_y * (termina_y + 1)) / 2;
 
         cout << sol << '\n';
