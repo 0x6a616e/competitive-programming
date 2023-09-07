12ef7a4a1a3d7a824ce51fda5c3eaf0325f19717
Jan <janel.fantasma2002@gmail.com>
2023-09-07 11:38:13 -0600

    feat: sí estaba muy cerca :(

--- done/CF1872D.cpp
+++ done/CF1872D.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+long long gcd(long long a, long long b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+long long lcm(long long a, long long b) { return a / gcd(a, b) * b; }
+
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
@@ -16,22 +26,10 @@ int main() {
 
         long long x_multiplos = n / x;
         long long y_multiplos = n / y;
-        long long interseccion = n / (x * y);
+        long long interseccion = n / lcm(x, y);
 
         x_multiplos -= interseccion;
         y_multiplos -= interseccion;
-
-        if (x % y == 0) {
-            x_multiplos = 0;
-            y_multiplos -= n / x;
-        }
-        if (y % x == 0) {
-            y_multiplos = 0;
-            x_multiplos -= n / y;
-        }
-
-        cout << n << " " << x << " " << y << " " << x_multiplos << " "
-             << y_multiplos << endl;
 
         if (x_multiplos) {
             long long termina_x = x_multiplos - 1;
