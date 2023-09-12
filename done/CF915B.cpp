a51ed13734f36324219ddcc24eb2914c154ff542
0x4a617a <janel.fantasma2002@gmail.com>
2023-09-11 20:02:54 -0600

    feat: ez dub

---
+++ done/CF915B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int n, pos, l, r;
+    cin >> n >> pos >> l >> r;
+
+    int sol = 0;
+    if (l > 1) ++sol;
+    if (r < n) ++sol;
+
+    if (pos < l) {
+        sol += l - pos;
+        pos = l;
+    } else if (pos > r) {
+        sol += pos - r;
+        pos = r;
+    } else if (pos > l && pos < r) {
+        if (l > 1 && r < n) {
+            if (abs(l - pos) <= abs(r - pos)) {
+                sol += pos - l;
+                pos = l;
+            } else {
+                sol += r - pos;
+                pos = r;
+            }
+        } else if (l > 1) {
+            sol += pos - l;
+            pos = l;
+        } else if (r < n) {
+            sol += r - pos;
+            pos = r;
+        }
+    }
+
+    if (pos == l && r < n) sol += r - l;
+    if (pos == r && l > 1) sol += r - l;
+
+    cout << sol << endl;
+
+    return 0;
+}
