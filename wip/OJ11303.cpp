55836f7fba7a9bf959ba6c31b6b70cf09f8fb96b
0x6a616e <janel.fantasma2002@gmail.com>
2023-09-26 00:10:21 -0600

    feat: wip

--- wip/OJ11303.cpp
+++ wip/OJ11303.cpp
@@ -2,18 +2,32 @@
 
 using namespace std;
 
+struct bigNum {
+    int base;
+    vector<int> digits;
+
+    bigNum(string num) {
+        base = 1000 * 1000 * 1000;
+        for (int i = (int)num.length(); i > 0; i -= 9)
+            if (i < 9)
+                digits.push_back(atoi(num.substr(0, i).c_str()));
+            else
+                digits.push_back(atoi(num.substr(i - 9, 9).c_str()));
+    }
+
+    void print() {
+        for (int &digit : digits) cout << digit << ' ';
+        cout << endl;
+    }
+};
+
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
 
-    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8};
+    bigNum bn("12345678901234567890123456789012345678901234567890");
 
-    int c = 0;
-    do {
-        cout << ++c << ": ";
-        for (int &i : nums) cout << i << ' ';
-        cout << endl;
-    } while (next_permutation(nums.begin(), nums.end()));
+    bn.print();
 
     return 0;
 }
