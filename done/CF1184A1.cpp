63c7bd142f18fb53f2853619fdee9d3d9a6fe841
0x6a616e <janel.fantasma2002@gmail.com>
2023-10-04 18:12:53 -0600

    feat: rechazado

---
+++ done/CF1184A1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    long long r;
+    cin >> r;
+
+    long long y;
+    for (long long x = 1; x * x + x + 1 <= r; ++x) {
+        y = r - x * x - x - 1;
+        if (x > y) break;
+        if (y % (2 * x) == 0) {
+            y /= 2 * x;
+            cout << x << ' ' << y << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO\n";
+
+    return 0;
+}
