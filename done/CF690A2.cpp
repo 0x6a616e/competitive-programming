847e39b2f26a0c635f63ad3d9dc06f98c5b2d6dc
0x6a616e <janel.fantasma2002@gmail.com>
2023-10-05 14:15:48 -0600

    w

---
+++ done/CF690A2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0) {
+        int jump = log2(n);
+        n -= (1 << jump);
+    }
+    cout << n / 2 << endl;
+
+    return 0;
+}
