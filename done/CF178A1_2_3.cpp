8e2ebb549222a06694929c97cf9aca38cf799d72
0x6a616e <janel.fantasma2002@gmail.com>
2023-10-04 20:03:11 -0600

    feat: posible

---
+++ done/CF178A1_2_3.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+
+    int a[n];
+    int jump = log2(n);
+    jump = (1 << (jump));
+
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    long long sol = 0;
+
+    for (int i = 0; i < n - 1; ++i) {
+        if (i + jump >= n) jump >>= 1;
+        sol += a[i];
+        a[i + jump] += a[i];
+        a[i] = 0;
+        cout << sol << '\n';
+    }
+
+    return 0;
+}
