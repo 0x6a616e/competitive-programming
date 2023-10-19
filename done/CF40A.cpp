674fb1591cb66236b2f8965241cdf7ec8c7b2188
0x6a616e <janel.fantasma2002@gmail.com>
2023-10-19 11:01:37 -0600

    feat: 1300

---
+++ done/CF40A.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int x, y;
+    cin >> x >> y;
+
+    char color;
+    if (x == 0 || y == 0)
+        cout << "black\n";
+    else {
+        if (x > 0 && y > 0) color = 'b';
+        if (x > 0 && y < 0) color = 'w';
+        if (x < 0 && y > 0) color = 'w';
+        if (x < 0 && y < 0) color = 'b';
+
+        double dist = sqrt(x * x + y * y);
+        while (dist > 1) {
+            color = (color == 'b' ? 'w' : 'b');
+            --dist;
+        }
+        if (dist == 1) color = 'b';
+        if (color == 'b') cout << "black\n";
+        if (color == 'w') cout << "white\n";
+    }
+
+    return 0;
+}
