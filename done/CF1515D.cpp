ff8d7c9a8d4e22df30ef1e5cf5b0bde19284a0b6
0x6a616e <janel.fantasma2002@gmail.com>
2023-10-27 10:00:21 -0600

    feat: chal 300

---
+++ done/CF1515D.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int t, n, l, r, c;
+    cin >> t;
+
+    while (t--) {
+        int sol = 0;
+        cin >> n >> l >> r;
+
+        map<int, int> izq, der;
+        for (int i = 0; i < l; ++i) {
+            cin >> c;
+            ++izq[c];
+        }
+
+        for (int i = 0; i < r; ++i) {
+            cin >> c;
+            if (izq[c])
+                --izq[c];
+            else
+                ++der[c];
+        }
+
+        int diff = abs(l - r);
+        if (diff) {
+            map<int, int> curr = (l > r ? izq : der);
+            map<int, int>::iterator it;
+            for (it = curr.begin(); it != curr.end() && diff; ++it) {
+                while (it->second >= 2 && diff) {
+                    ++sol;
+                    diff -= 2;
+                    it->second -= 2;
+                    if (l > r) izq[it->first] -= 2;
+                    if (r > l) der[it->first] -= 2;
+                }
+            }
+        }
+
+        if (diff) {
+            map<int, int> curr = (l > r ? izq : der);
+            map<int, int>::iterator it;
+            for (it = curr.begin(); it != curr.end() && diff; ++it) {
+                while (it->second >= 1 && diff) {
+                    ++sol;
+                    diff -= 2;
+                    --it->second;
+                    if (l > r) {
+                        --izq[it->first];
+                        ++der[it->first];
+                    } else {
+                        --der[it->first];
+                        ++izq[it->first];
+                    }
+                }
+            }
+        }
+
+        map<int, int>::iterator it;
+        for (it = izq.begin(); it != izq.end(); ++it) sol += it->second;
+
+        cout << sol << '\n';
+    }
+
+    return 0;
+}
