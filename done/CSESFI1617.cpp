e684c48602e2454da57284f69483c0a20a21e117
0x6a616e <janel.fantasma2002@gmail.com>
2023-11-02 08:15:42 -0600

    feat: ez

---
+++ done/CSESFI1617.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+
+    cout << binpow(2, n, 1000000007) << endl;
+
+    return 0;
+}
