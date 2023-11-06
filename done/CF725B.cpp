57a56a68d3eda1965d59d7c99157f3513a07059f
0x6a616e <janel.fantasma2002@gmail.com>
2023-11-05 22:08:20 -0600

    feat: chal 100

---
+++ done/CF725B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    long long n;
+    char s;
+
+    cin >> n >> s;
+
+    long long base = (n - 1) / 4;
+    long long sol = base * 16;
+    long long b = 4 * base + 1;
+
+    if (n != b && n != b + 2) sol += 7;
+
+    for (char c : {'f', 'e', 'd', 'a', 'b', 'c'}) {
+        ++sol;
+        if (s == c) break;
+    }
+
+    cout << sol << endl;
+
+    return 0;
+}
