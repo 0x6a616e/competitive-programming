4866b97d8786ce96022ff215b7812a97ea0c8991
0x6a616e <janel.fantasma2002@gmail.com>
2023-11-09 12:42:10 -0600

    feat: ez

---
+++ done/CF1894A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        cout << s[n - 1] << '\n';
+    }
+
+    return 0;
+}
