0ea532dceb8a1701dc2aa6fed2add279a54dda62
0x6a616e <janel.fantasma2002@gmail.com>
2024-01-09 09:21:27 -0600

    chal: 100

---
+++ done/CF989B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int n, p;
+    cin >> n >> p;
+
+    string s;
+    cin >> s;
+
+    bool sol = false;
+
+    for (int i = p; i < n && !sol; ++i) {
+        if (s[i] == '.') {
+            sol = true;
+            if (s[i - p] == '.') {
+                s[i - p] = '0';
+                s[i] = '1';
+            } else {
+                s[i] = s[i - p] == '0' ? '1' : '0';
+            }
+        } else {
+            if (s[i - p] != '.' && s[i] != s[i - p]) sol = true;
+        }
+    }
+
+    for (int i = 0; i + p < n && !sol; ++i) {
+        if (s[i] == '.') {
+            sol = true;
+            if (s[i + p] == '.') {
+                s[i + p] = '0';
+                s[i] = '1';
+            } else {
+                s[i] = s[i + p] == '0' ? '1' : '0';
+            }
+        } else {
+            if (s[i + p] != '.' && s[i] != s[i + p]) sol = true;
+        }
+    }
+
+    if (sol) {
+        for (char &c : s)
+            if (c == '.')
+                cout << '0';
+            else
+                cout << c;
+        cout << '\n';
+    } else {
+        cout << "NO\n";
+    }
+
+    return 0;
+}
