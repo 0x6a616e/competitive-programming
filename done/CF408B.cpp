e12165aa78a5aafed4043a3a1729abd6a9d26310
0x6a616e <janel.fantasma2002@gmail.com>
2024-01-09 19:55:13 -0600

    chal: 100

---
+++ done/CF408B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    string n, m;
+    cin >> n >> m;
+
+    vector<int> cont1(27, 0);
+    vector<int> cont2(27, 0);
+
+    for (char &c : n) ++cont1[c - 'a'];
+    for (char &c : m) ++cont2[c - 'a'];
+
+    bool posible = true;
+    int sol = 0;
+
+    for (int i = 0; i < 27 && posible; ++i) {
+        if (cont2[i] && !cont1[i]) posible = false;
+        sol += min(cont1[i], cont2[i]);
+    }
+
+    if (posible)
+        cout << sol << '\n';
+    else
+        cout << "-1\n";
+
+    return 0;
+}
