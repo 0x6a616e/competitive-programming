31d910f0519734c218e3bb89b543c1d31b9bbba2
0x6a616e <janel.fantasma2002@gmail.com>
2024-02-22 13:31:47 -0600

    CSES: Dice Combinations

--- done/CSESFI1633.cpp
+++ done/CSESFI1633.cpp
@@ -1,4 +1,4 @@
-// https://cses.fi/problemset/task/1633
+// https://cses.fi/problemset/task/1633/
 
 #include <bits/stdc++.h>
 
@@ -6,29 +6,22 @@ using namespace std;
 
 const int MOD = 1000000007;
 
-unsigned long long solve(vector<unsigned long long> &memo, int n) {
-    if (memo[n] == -1) {
-        if (n == 0) {
-            memo[n] = 1;
-        } else {
-            unsigned long long sol = 0;
-
-            for (int i = 1; i <= min(6, n); ++i) sol += solve(memo, n - i);
-
-            memo[n] = sol % MOD;
-        }
-    }
-    return memo[n];
-}
-
 int main() {
-    cin.tie(NULL);
+    cin.tie(0);
     ios::sync_with_stdio(0);
 
     int n;
     cin >> n;
-    vector<unsigned long long> memo(n + 1, -1);
-    cout << solve(memo, n) << endl;
+
+    int memo[n + 1];
+    memo[0] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        memo[i] = (i <= 6 ? 1 : 0);
+        for (int j = max(0, i - 6); j < i; ++j) memo[i] = (memo[i] + memo[j]) % MOD;
+    }
+
+    cout << memo[n] << '\n';
 
     return 0;
 }
